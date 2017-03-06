069f5f0804f393c9aa114dd390ac3ffedff71485
Brian-Lu <brianlu22@yahoo.com>
2017-03-05 23:45:42 -0500

    Finish

--- vector.js
+++ vector.js
@@ -1,23 +1,38 @@
-var clear = document.getElementById("clear");
+var clearbtn = document.getElementById("clear");
 var svg = document.getElementById("savage");
-var currentX;
-var currentY;
+var currentX = -22;
+var currentY = -22;
 
-
-svg.addEventListener("mousemove", function(e){
-    currentX = e.offsetX;
-    currentY = e.offsetY;
-});
-
-var clearSVG = function() {
-    while (svg.hasChildNodes()) {
+var clearStuff = function(e) {
+    currentX = -1;
+    currentY = -1;
+    while(svg.lastChild) {
         svg.removeChild(svg.lastChild);
     }
 };
+clearbtn.addEventListener("click", clearStuff);
 
-var connectTheDots = function(e) {
-  if(svg.hasChildNodes()) {
-
-  }
+var addStuff = function(e) {
+    if (currentX < 0) {
+	    currentX = e.offsetX;
+	    currentY = e.offsetY;
+    }
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", currentX);
+    line.setAttribute("y1", currentY);
+    currentX = e.offsetX;
+    currentY = e.offsetY;
+    line.setAttribute("x2", currentX);
+    line.setAttribute("y2", currentY);
+    line.setAttribute("style", "stroke:grey;stroke-width:1");
+    svg.appendChild(line);
+    c.setAttribute("cx", currentX);
+    c.setAttribute("cy", currentY);
+    c.setAttribute("r", 10);
+    c.setAttribute("stroke","grey");
+    c.setAttribute("stroke-width",1);
+    c.setAttribute("fill", "grey");
+    svg.appendChild(c);
 };
-clear.addEventListener("click", clearSVG);
+svg.addEventListener("click", addStuff);
